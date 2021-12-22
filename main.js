42c26201c1abe8941cc5e92bf77d6c3dd6a62aeb
Koras02 <wjdgh0727@gmail.com>
2021-12-22 22:48:11 +0900

    자바스크립트 2.콘솔에 출력

---
+++ main.js
@@ -0,0 +1,30 @@
+// 순수 바닐라 js를 사용할때 사용 
+// Javscript is very flexible
+// flexble == dangers 
+// 유용하다는 것은 실수할 수 있다는 뜻도 된다.
+// added ECMAScript5
+
+'use strict';
+
+// node main.js == Hello world 화면에 출력
+// node.js와 web apis둘다 console 에 관련된 api 존재
+// 그리고 둘다 api interface가 동일
+console.log('Hello world!!');
+
+
+// api (Application Programing Interface)
+// web api javescript언어에 포함된 api가 아닌 브라우저가 읽을 수 있는 함수 
+
+// console은 언어자체 포함된 아이 x 통상적으로 쓰는에라 nodejs api에 포함되
+// 똑같은 console로 nodejs를 호출 
+
+
+// 'use-script' 없이 선언하면 문제 x 
+// 선언시 o
+let a;
+
+a = 6;
+
+
+
+
